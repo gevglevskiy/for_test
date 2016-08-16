7f9f7e7fb27c37aae7da2c0c201d92e890ec1974
Grigoriy Evglevskiy <gevglevskiy@gmail.com>
2016-08-16 13:22:46 +0300

    first commtit

---
+++ js/common.js
@@ -0,0 +1,37 @@
+(function() {
+
+//var buttons = document.getElementsByTagName('button');
+
+var testFunc = function (e) {
+  alert('Создали документ');
+};
+
+var changeColor = function (e) {
+
+  eventsObj.preventDefault(e);
+
+  var elem = eventsObj.getTarget(e),
+  colorData = elem.getAttribute('data-color');
+
+  if (colorData) {
+    if (e.type === 'click') {
+      document.body.className = '';
+    } else if (e.type === 'mouseover') {
+      document.body.className = colorData;
+    }
+  }
+};
+
+eventsObj.addEvent(document,'click', changeColor);
+eventsObj.addEvent(document,'mouseover', changeColor);
+
+// for (var i = 0, len = buttons.length; i < len; i++) {
+//
+//  //buttons[i].addEventListener('click', changeColor, false);  // модель событий в ie
+//
+//   eventsObj.addEvent(buttons[i],'click', changeColor);  // кроссбраузерная модель
+//   //eventsObj.removeEvent(buttons[i],'click', changeColor);  // кроссбраузерная модель
+//
+// }
+
+})();
---
+++ js/events.js
@@ -0,0 +1,39 @@
+
+var eventsObj = {
+  addEvent: function (el, type, fn) {
+    if (typeof addEventListener !== 'undefined') {
+      el.addEventListener(type, fn, false);
+    } else if (typeof attachEvent !== 'undefined') {
+      el.attachEvent('on' + type, fn);
+    } else {
+      el['on' + type] = fn;
+    }
+  },
+
+  getTarget: function (event) {
+    if (typeof event.target !== 'undefined') {
+      return event.target;
+    } else {
+      return event.srcElement;
+    }
+  },
+
+  removeEvent: function (el, type, fn) {
+      if (typeof removeEventListener !== 'undefined') {
+        el.removeEventListener(type, fn, false);
+      } else if (typeof detachEvent !== 'undefined') {
+        el.detachEvent('on' + type, fn);
+      } else {
+        el['on' + type] = null;
+      }
+  },
+
+  preventDefault: function (event) {
+    if (typeof event.preventDefault!== 'undefined') {
+      event.preventDefault();
+    } else {
+      event.returnValue = false;
+    }
+  }
+
+};
